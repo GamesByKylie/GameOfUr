908531eba441e5683cfb791cb33f4b7bc2aff4ee
Kylie <kr.gilde@gmail.com>
2022-05-22 18:20:53 -0400

    Edited a bunch of the UI to scale better. It's not 100% - specifically, the TextMeshPros in the menus still need to be scaled in code to figure out what their preferred width should be. Also, I wasn't able to fix or even replicate the null reference error in changing resolutions. It's still present in the build but not in the editor, so more testing is needed. Also to-do: make a list of set resolutions (or aspect ratios) to check for the resolution dropdown so it stops giving really weird and obscure resolutions no one actually uses.

---
+++ Ur/Assets/Scripts/UI/MenuLineRenderer.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuLineRenderer : MonoBehaviour
+{
+    public RectTransform line;
+    public RectTransform pointAt;
+    public float extraLength;
+
+    private Vector2 screenSize;
+
+    private void Start()
+    {
+        ResizeLine();
+    }
+
+    private void Update()
+    {
+        if (Screen.width != screenSize.x || Screen.height != screenSize.y)
+        {
+            Debug.Log("Screen size changed");
+            ResizeLine();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        screenSize = new Vector2(Screen.width, Screen.height);
+    }
+
+    public void ResizeLine()
+    {
+        Vector3 dir = pointAt.position - line.position;
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        line.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+
+}
---
+++ Ur/Assets/Scripts/UI/ParticleMatchScale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleMatchScale : MonoBehaviour
+{
+    public ParticleSystem ps;
+    public RectTransform match;
+
+    private void OnEnable()
+    {
+        ParticleSystem.MainModule psMain = ps.main;
+        float aspect = Screen.width * 1f / Screen.height;
+        psMain.startSizeX = match.rect.width;
+        psMain.startSizeY = match.rect.height;
+    }
+}
